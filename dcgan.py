6be93e14b594141eb3110c9bcbaa57ab4bbe092d
Mohit Jain <mohitjain1999@yahoo.com>
2018-12-12 22:37:17 +0400

    Create discriminator of the dcgan.

--- dcgan.py
+++ dcgan.py
@@ -44,3 +44,34 @@ class Generator(nn.Module):
 		x = f.tanh(self.tconv5(x))
 
 		return x
+
+class Discriminator(nn.Module):
+	def __init__(self, params):
+		super().__init__()
+
+		self.conv1 = nn.Conv2d(params['nc'], params['ndf'],
+			4, 2, 1, bias=False)
+
+		self.conv2 = nn.Conv2d(params['ndf'], params['ndf']*2,
+			4, 2, 1, bias=False)
+		self.bn2 = nn.BatchNorm2d(params['ndf']*2)
+
+		self.conv3 = nn.Conv2d(params['ndf']*2, params['ndf']*4,
+			4, 2, 1, bias=False)
+		self.bn3 = nn.BatchNorm2d(params['ndf']*4)
+
+		self.conv4 = nn.Conv2d(params['ndf']*4, params['ndf']*8,
+			4, 2, 1, bias=False)
+		self.bn4 = nn.BatchNorm2d(params['ndf']*8)
+
+		self.conv5 = nn.Conv2d(params['ndf']*8, 1, 4, 1, 0, bias=False)
+
+	def forward(self, x):
+		x = F.leaky_relu(self.conv1(x), 0.2, True)
+		x = F.leaky_relu(self.bn2(self.conv2(x)), 0.2, True)
+		x = F.leaky_relu(self.bn3(self.conv3(x)), 0.2, True)
+		x = F.leaky_relu(self.bn4(self.conv4(x)), 0.2, True)
+
+		x = F.sigmoid(self.conv5(x))
+
+		return x
